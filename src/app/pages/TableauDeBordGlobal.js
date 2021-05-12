dde1a83e6a2e7388b5dbdc4e1fb4d2dd0dbc18c8
Eric Kandja <eric.kandja@gmail.com>
2021-05-11 22:30:19 -0400

    Tableau de bord global - Creation boutoun

--- src/app/pages/TableauDeBordGlobal.js
+++ src/app/pages/TableauDeBordGlobal.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { Card } from 'react-bootstrap';
 import { CarteGroupeActif } from "./composants/communs/CarteGroupeActif";
-import { ZoneEcrireMessage} from  "./composants/communs/ZoneEcrireMessage"
+import { ZoneEcrireMessage } from "./composants/communs/ZoneEcrireMessage";
+import { ButtonSite } from './composants/communs/ButtonSite';
 import './css/styleTableauBordGlobal.css';
 
 
@@ -13,11 +14,29 @@ export class TableauDeBordGlobal extends React.Component {
 
     render() {
 
+        const messageButtonColor = {
+            color: "#8950FC",
+            fontSize: "12px",
+            fontWeight: "600",
+            
+                
+        };
+        
+        const bgMessageButton = {
+            background: "#EEE5FF",
+            border: "1px solid rgb(238, 229, 255)",
+            
+        }
+        
+
         return (
             <>
                 <div className="d-flex flex-column flex-xl-row ml-5">
                     <div className="flex-lg-row-fluid ">
-                        <h2 class="h2StagiaireActif">Stages actifs</h2>
+                        <div className="row">
+                        <div className="col-6"><h2 class="h2StagiaireActif">Stages actifs</h2></div>
+                        <div className="buttonMessage col-6" align="right"><ButtonSite messageButton="Créer un nouveau message" messageButtonColor={messageButtonColor} bgMessageButton={bgMessageButton} /></div>
+                        </div>
                         <CarteGroupeActif />
                         <h2 class="h2StagiaireActif">Annonce rapide</h2>
                         <ZoneEcrireMessage />
