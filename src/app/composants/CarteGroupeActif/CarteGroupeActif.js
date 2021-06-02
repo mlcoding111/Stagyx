814a61f152602aca3e4aae1d3992d8de0c187b0f
Eric Kandja <eric.kandja@gmail.com>
2021-06-02 07:13:24 -0400

    Ajout du rapport de stage

--- src/app/composants/CarteGroupeActif/CarteGroupeActif.js
+++ src/app/composants/CarteGroupeActif/CarteGroupeActif.js
@@ -4,9 +4,6 @@ import { Row, Col, Card } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
 
 export class CarteGroupeActif extends React.Component {
-
-
-
     render() {
         return (
             <>
@@ -33,7 +30,7 @@ export class CarteGroupeActif extends React.Component {
                     <Card>
                         <Card.Body>
                             <Card.Title><h2>GROUPE {this.props.groupeNumber}</h2></Card.Title>
-                            <Card.Subtitle className="mb-5 text-muted">Développement web M.W56</Card.Subtitle>
+                            <Card.Subtitle className="mb-5 text-muted"> Développement web M.W56</Card.Subtitle>
                             <Card.Text>
                                 <div className="stageActifAttribut">Début : <span className="stageActifValue">2015-02-25</span></div>
                                 <div className="stageActifAttribut">Fin : <span className="stageActifValue">2015-03-25</span></div>
